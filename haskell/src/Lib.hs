96df4e0554a01b392ec5d9d7c9c286903ff4e798
psilospore <syedjafri99@gmail.com>
2024-10-26 16:21:01 -0400

    Syncing with Quinn

--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -31,28 +31,6 @@ import Data.Set (Set, cartesianProduct, union)
 -- Gω G\omega Really G with a super script ω (omega)
 -- Many of these are described in section 3 Preliminaries
 
--- | MDP States
-data S = S Int
-
--- | MDP Booleans
-data A = A Bool
-
--- | Probablistic Transition Function
-type P = (S, A) -> Set (S, Float)
-
--- | Reward funtion
-type R = (S, A, S) -> Float
-
--- | Markov Decision Process
--- I wonder if we even need this given they start talking about an abstraction over the MDP φ
-data M = M {
-  _S :: Set S
-  , sᵢ :: S -- Initial state
-  , _A :: Set A -- Finite set of boolean actions
-  , _P :: P
-  , _R :: R
-}
-
 -- Automata States
 data Q = Q Int deriving (Show, Eq, Ord)
 
@@ -66,76 +44,51 @@ data S _Σᵢ¹ _Σᵢ² _Σ₀ = S {
   -- Initial state
   , q₀ :: Q
   -- Input alphabet
-  , _Σᵢ :: _Σᵢ
+  , _Σᵢ :: (_Σᵢ¹, _Σᵢ²)
   -- Output alphabet
   , _Σ₀ :: _Σ₀
   -- Transition function
-  , δ :: (Q, _Σᵢ) → _Σᵢ
+  , δ :: (Q, (_Σᵢ¹, _Σᵢ²)) → Q
   -- Complete output function
-  , λ :: (Q, _Σᵢ) → _Σ₀
+  , λ :: (Q, (_Σᵢ¹, _Σᵢ²)) → _Σ₀
 }
 
 -- Safety automation φˢ
 -- The System S satisfies the automation if the run of S only visits safe states in F
-data SafetyAutomation _Σᵢ¹ _Σᵢ² = SafetyAutomation {
+-- Σᵢ = Σᵢ¹ x Σᵢ² but can also be decomposed to TODO
+data SafetyAutomaton _Σᵢ¹ _Σᵢ² = SafetyAutomaton {
   _Q :: Set Q -- States
   , q₀:: Q
-  , _Σ :: _Σ
+  , _Σ :: Set (_Σᵢ¹, _Σᵢ²) -- Input alphabet
   , δ :: (Q, (_Σᵢ¹, _Σᵢ²)) -> Q
   , _F :: Set Q -- Set of safe states where F ⊆ Q
   }
 
 -- | MDP Abstraction φᵐ
 -- Which is some abstraction over the MDP
--- The confusing part is that they state the abstraction is also a Safety word automation so like SafetyAutomation φˢ?
--- Maybe that's fine it's just an automata but can we assume we know the safe states F? Perhaps I guess?
-data MDPAbstraction = MDPAbstraction {
+-- Σᵢ = A x L
+data MDPAbstraction _Σᵢ¹ _Σᵢ² = MDPAbstraction {
   _Q :: Set Q
   , q₀ :: Q
-  , _Σ :: _Σ
-  , δ :: (Q, _Σ) -> Q
+  , _Σᵢ :: Set (_Σᵢ¹, _Σᵢ²)
+  , δ :: (Q, (_Σᵢ¹, _Σᵢ²)) -> Q
   , _F :: Set Q
 }
 
+-- | LTL to Safety Automaton
+-- TODO
+ltlToAutomaton :: LTL -> SafetyAutomaton
+ltlToAutomaton = undefined
 
 -- | 2 player Game
-data Game _G = Game {
+data Game _G _Σᵢ _Σₒ = Game {
     _G :: Set _G -- Finite set of game states
     , g₀ :: _G -- Initial state
     , _Σᵢ :: _Σᵢ -- Input alphabet
     , _Σₒ :: _Σₒ -- Output alphabet
-    , δ :: (_G, Σᵢ, Σₒ) -> _G -- Transition function
-    , win :: _Gω
+    , δ :: (_G, _Σᵢ, _Σₒ) -> _G -- Transition function
+    , win :: _G -- The winning region
 }
-
--- | Used for synthesizing the shield
--- Defines a set Fᵍ  ⊆ G of safe states
--- Where win(g₀, g₁, ...) iff ∀i ≥ 0. gᵢ ∈ Fᵍ
--- I think we can solve for this using SMT. In the original implementation they used BDDs.
--- safetyGame ∷ G -> G
--- safetyGame g = undefined
-
-
-safetyGame ∷ S _Σᵢ¹ _Σᵢ² _Σ₀ -> SafetyAutomation -> S _ _
-safetyGame=
-    let g = undefined
-        q = undefined
-        _Σᵢ = undefined
-        _Σₒ= undefined
-        δ' = undefined -- (g, σᵢ ) = (g, σᵢ, ρ(g, σᵢ))
-        ρ = undefined
-    in undefined -- TODO S q _Σᵢ _Σₒ δ'
-
--- | Uses the safety game to synthesize a shield which implements the winning strategy in a new reactive system (Is it a finite reactive system?)
--- shield  ∷ Shield _Σᵢ _Σ₀
--- shield =
---     let g = undefined
---         q = undefined
---         _Σᵢ = undefined
---         _Σₒ= undefined
---         δ' = undefined -- (g, σᵢ ) = (g, σᵢ, ρ(g, σᵢ))
---         ρ = undefined
---     in undefined -- TODO S q _Σᵢ _Σₒ δ'
 
 type Σ = Set Int
 
@@ -143,9 +96,6 @@ type Σ = Set Int
 -- e.g.  {level < 1, 1 ≤ level ≤ 99, level > 99}
 -- type Σᵢ¹ = L
 
--- A but maybe just for preemtive shields
--- e.g. {Open, Close}
-type Σᵢ² = A
 
 -- | Propositions TODO not sure how to represent this
 type Prop = String
@@ -172,8 +122,6 @@ data LTL
 exampleLtlFormula :: LTL
 exampleLtlFormula = G ( AP "r" ||| X (AP "g"))
 
-waterTankExampleLtlFormula = G (AP "level > 0") &&& G (AP "level < 100")
-   -- TOOD &&& G ((AP "open" &&& X (AP "close")) --> (X X (AP "close") &&& XXX (AP "close")))
 
 -- | A trace I think?
 type Trace = [Set Prop]
@@ -201,94 +149,76 @@ satisfies σ idx formula = case formula of
   U f1 f2 ->
     let future = drop idx σ
         holdsAt i = satisfies σ i f2 && all (\j -> satisfies σ j f1) [idx .. i - 1]
-    in any (\i -> holdsAt i) [idx .. length σ - 1]
+    in any holdsAt [idx .. length σ - 1]
 
--- | Preemptive Shield iteration
--- Given the time step compute a set of safe actions (remove unsafe actions)
--- Environment executes one action and moves to the next state providing a reward
--- I think the paper describes this more clearly even though this might be harder to use
--- The input alphabet Σᵢ = Σᵢ¹ x Σᵢ² = L x A
--- The output alphabet Σₒ = 2ᴬ
--- Where A is the boolean 'Actions' in the MDP
--- preemptiveShieldIter :: (L, A) -> Set A
--- preemptiveShieldIter (l, a) = undefined
-
--- This is actually properties of the shield maybe not what we want
--- But overall it can also be seem as transforming a MDP into a new MDP (section 5.1)
-preemptiveShield :: M -> Shield -> M
-preemptiveShield _M _S =
-  -- Product of the original MDP and the state space of the shield
-  let _S' = undefined
-      -- For each s' in S' create a new subset of available actions
-      -- Apply the shield to Aₛ to get A'ₛ
-      -- So that gets us a bunch of A'ₛ but how do we get A'?
-      _A' = undefined
-      -- We only want transition functions from P for actions in A'ₛ
-      _P' = undefined
-      _R' = undefined
-  in M _S' M.s_i  _A' _P' _R'
 
 -- | Section 6 a shield is computed from an abstraction of the MDP φᵐ and the safety automaton φˢ
--- 1. Translate φˢ and φᵐ into a safety game
-
-computeShield :: SafetyGame -> MDPAbstraction -> S
-computeShield φˢ φᵐ =
+computePreemptiveShield :: SafetyAutomaton -> MDPAbstraction -> S
+computePreemptiveShield φˢ φᵐ =
   -- 1. Translate φˢ and φᵐ into a safety game
-  -- TODO this is similar to shieldpy
-
-  -- The MDP abstraction's Σᵢ = A x L
+  -- The MDP abstraction's Σᵢ = A x L therefore:
   let _A = fst <$> φᵐ._Σᵢ -- Actions
       _L = snd <$> φᵐ._Σᵢ -- Labels
       _G = Set.cartesianProduct φˢ._Q φᵐ._Q -- A product of both automata's states
       _G' = SafetyGame {
             _G = φˢ._Q `cartesianProduct` φᵐ._Q
             , q₀ = (φˢ.q₀, φᵐ.q₀)
-            , Σᵢ = _L
-           ,  Σₒ = _A
+            , _Σᵢ = _L
+           ,  _Σₒ = _A
            -- Paper might be missing a qₘ I think
            -- I wonder if I should use tuples instead of functions like in python
            -- Not sure what is better when I start using an SMT solver
-           , δ = ((q, qₘ), l :: L, a :: A) -> (φˢ.δ q (l, a), φᵐ.δ qₘ, (l, a))
-           , Fᵍ = (φˢ._F `cartesianProduct` φᵐ.Q) `union` (φˢ.Q `cartesianProduct` (φᵐ._F \\ φᵐ._F))
+           , δ = \(q, qₘ) l a -> (φˢ.δ q (l, a), φᵐ.δ qₘ, (l, a))
+           , _Fᵍ = (φˢ._F `cartesianProduct` φᵐ.Q) `union` (φˢ.Q `cartesianProduct` (φᵐ._F \\ φᵐ._F))
           }
--- 2. Compute the winning strategy TODO this is described in Shield Synthesis but I think we can use SMT for this part
-    _W = undefined
--- 3. For pre-emptive shielding translate G and W into a reactive system
-    in Shield {
-      _Q = _G
-      , q₀ = _G'.q₀
-      , _Σᵢ = φᵐ._Σᵢ -- A x L which we can get from the MDP abstraction
-      -- 2ᴬ so I think it's a powerset? Might make sense because it's going to suggest a set of safe actions
-      -- Or it's a mapping of Action to {0, 1} where 0 is unsafe and 1 is safe?
-      , _Σₒ = powerset _A
-      , δ = (g, l, a) -> _G'.δ (g, l, a)
-      , λ = (g, l, a) -> if g ∈ _W  -- TODO yeah actually make it  a tuple or Maybe
-    }
-
+      -- 2. Compute the winning strategy TODO this is described in Shield Synthesis but I think we can use SMT for this part
+      _W = undefined
+      -- 3. For pre-emptive shielding translate G and W into a reactive system
+      _S = Shield {
+        _Q = _G
+        , q₀ = _G'.q₀
+        -- A x L which we can get from the MDP abstraction
+        , _Σᵢ = φᵐ._Σᵢ
+        -- The output is a set of actions 2ᴬ
+        , _Σₒ = powerset _A
+        , δ = \(g, l, a) -> _G'.δ (g, l, a)
+        , λ = \(g, l) -> Set.filter (\a -> φˢ.δ (g, l, a) `elem` _W) _A
+      }
+  in prune _S
 
 -- Optionally remove states that are not reachable from the initial state
 prune :: S -> S
-prune s = s -- TODO
-
-
+prune s = s -- TODO optional just going to return the input for now
 
 -- Label set e.g. {level < 1, 1 ≤ level ≤ 99, level > 99}
 type L = Set Prop
 
+-- Examples --
 
 -- | Water Tank Example
-watertankL :: L
-watertankL = Set.fromList ["level < 1", "1 ≤ level ≤ 99", "level > 99"]
+-- watertankL :: L
+-- watertankL = Set.fromList ["level < 1", "1 ≤ level ≤ 99", "level > 99"]
+
+data WatertankAP = Open | Close deriving (Show, Eq, Ord)
+
+-- | Water tank actions
+newtype WatertankA = WatertankA { unWatertankA :: Set WatertankAP } deriving (Show, Eq, Ord)
+
+data WatertankL = LevelLessThan1 | LevelBetween1And99 | LevelGreaterThan99 deriving (Show, Eq, Ord)
+
+-- TODO maybe make atomic propositions something else
+-- So I can draw connections
+waterTankExampleLtlFormula = G (AP "level > 0") &&& G (AP "level < 100")
+   -- TOOD &&& G ((AP "open" &&& X (AP "close")) --> (X X (AP "close") &&& XXX (AP "close")))
 
 -- waterTankA :: A
 -- waterTankA = Set.fromList [ "opened", "closed"]
 
-
--- | Abiter example from the shortened paper
+-- | Arbiter example from the shortened paper
 -- This seems to be a request based system where you can request A or B
 
 data ArbiterΣᵢ = RequestARequestB | DenyARequestB | RequestADenyB | DenyADenyB deriving (Show, Eq, Ord)
 
 data ArbiterΣₒ = GrantedAGrantedB | DeniedAGrantedB | GrantedADeniedB | DeniedADeniedB deriving (Show, Eq, Ord)
 
-data ArbiterQ = QIdle | QGrantedA | Q GrantedB deriving (Show, Eq, Ord)
+data ArbiterQ = QIdle | QGrantedA | QGrantedB deriving (Show, Eq, Ord)
